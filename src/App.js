0797a5e52918b306db0cddac73f6a8c0b3c4ff7d
jkwon <kwonejk@gmail.com>
2022-05-12 22:41:23 +0800

    show video list with title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import VidoeList from './components/video_list/video_list';
 
 function App() {
+  const [videos, setVideos] = useState([]); 
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCJQAxdtjRaWpmRmEujcXrardcJ3uWMkNU", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []); 
+
+
+
   return (
-   <h1>HEllo!</h1>
+    <VidoeList videos={videos} />
   );
 }
 
